2383dedaed5026ff753f4f17bbf9bac8e7b29dcf
Alexandre Lira <alexandrelira112@gmail.com>
2022-06-01 20:58:10 -0300

    Page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,33 @@
-import styles from '../styles/home.module.scss'
 import Head from 'next/head'
+import Image from 'next/image'
+import { SubscribeButton } from '../components/SubscribeButton/SubscribeButton'
+import styled from './home.module.scss'
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ig.news</title>
+        <title>Home | Ig.news</title>
       </Head>
-      <div>
-        <h1 className={styles.title}>Hello World</h1>
-      </div>
+
+      <main className={styled.contentContainer}>
+        <section className={styled.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <br />
+            the <span> React </span>
+            world
+          </h1>
+
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+
+        </section>
+
+        <Image src="/images/avatar.svg" alt="Girl coding" width='500px' height='500px' />
+      </main>
     </>
   )
 }
