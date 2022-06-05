515faacfa6b7d4589b1c2a2d9c088c22fab038b0
Alexandre Lira <alexandrelira112@gmail.com>
2022-06-05 12:31:05 -0300

    writing the user on database using faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { signIn } from "next-auth/react"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,4 +15,37 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              { data: { email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+
+    },
+  }
 })
