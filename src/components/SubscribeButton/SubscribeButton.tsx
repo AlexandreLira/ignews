5787ba009ad2d7f2ba629b4f461cfa474408f13c
Alexandre Lira <alexandrelira112@gmail.com>
2022-06-05 17:24:29 -0300

    setup stripe webhooks

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styled from './subscribeButton.module.scss'
 
 
@@ -6,10 +9,37 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+    const { data: session } = useSession()
+
+    async function handleSubscribe() {
+        if (!session) {
+            signIn('github')
+            return;
+        }
+        try {
+
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId})
+
+        } catch (err) {
+            alert(err.message)
+        }
+        // criação da checkout session
+
+
+    }
+
     return (
-        <button 
+        <button
             type="button"
             className={styled.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
